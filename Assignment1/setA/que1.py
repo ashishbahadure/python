0c9a43c7c1efcaba307a7690c34e56540dd22034
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-04-21 17:22:43 +0530

    Update que1.py

--- Assignment1/setA/que1.py
+++ Assignment1/setA/que1.py
@@ -2,23 +2,22 @@
 # if any of the values entered are duplicates otherwise it print " All Unique"
 # Example Let 5 integers are 32,45,90,45,6 then output "Duplicates " to be printed
 
-a = [1,2,3,4,1]
-print(a)
-k = 0
+
+a = [1,2,3,4,5]
+counter = 0
 print(len(a))
 for i in range(0,len(a)):
-    #print("i",i)
     for j in range(i+1,len(a)):
-       # print("j",j)
         if(a[i] == a[j]):
-            print(a[i],"Dublicate")
-            k+=1
+            print("Dublicate")
+            counter+=1
 
-print("i",i)
-print("j",j)
-print("k",k)
-if(k==0):
-    print("All UNIque");
+
+if(counter==0):
+    print("all unique");
+
+
+    
 
 
     
