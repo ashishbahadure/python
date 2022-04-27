55bee7e11c6cd17e3c23e98f0cc306f6f045b0f5
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-04-27 11:27:19 +0530

    Update que1.py

--- Assignment1/setC/que1.py
+++ Assignment1/setC/que1.py
@@ -1,2 +1,15 @@
 # Write a binary search function which search an item in a sorted list
 # The function should return the index of element to be searched in the list
+# Write a binary search function which search an item in a sorted list
+# The function should return the index of element to be searched in the list
+
+a=[1,2,3,4,5]
+n = int(input("Enter number"))
+
+for i in range(0,len(a)):
+    # print(a[i])
+    if(n == a[i]):
+        print("The number is present in the list at index ",i);
+        break;
+    else:
+        print("The number is not present in the list")
